75a2c0a6decfd38ef5641dc809ba86675d48b757
ENNA-Egor <e.yudakov@gmail.com>
2025-10-03 09:17:25 +0300

    Добавил  invoke. Пока не получается надо смотреть почему?

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 
 const path = require('node:path')
- console.log (__dirname)
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,11 +10,9 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
- 
-
-  win.loadFile('index.html')
+  win.loadFile('src/index.html')
 }
-
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 })
